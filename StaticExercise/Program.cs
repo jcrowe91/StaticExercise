bf6a2576be7b7987e3c2a19bac369206f6624395
jcrowe91 <jcrowe9119@gmail.com>
2022-01-01 14:25:19 -0600

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = TempConverter.CelsiusToFahrenheit(50);
+            double fahrenheit = TempConverter.FahrenheitToCelsius(100);
+
+            Console.WriteLine($"Celsius: {celsius}!");
+            Console.WriteLine($"Fahrenheit: {fahrenheit}!");
+        }
+
+
+
+    }
+}
